e2630f80f05c1d1fff38c375d8a4c410dbc65e29
me_how :) <michcio113@gmail.com>
2024-02-05 12:37:00 +0100

    app setup

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,53 @@
+package org.example;
+
+
+import org.example.answer.*;
+
+import java.io.FileNotFoundException;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    static Day1 day1 = new Day1();
+    static Day2 day2 = new Day2();
+    static Day3 day3 = new Day3();
+    static Day4 day4 = new Day4();
+    static Day5 day5 = new Day5();
+    static Day6 day6 = new Day6();
+    static Day7 day7 = new Day7();
+    static Day8 day8 = new Day8();
+    static Day9 day9 = new Day9();
+    static Day10 day10 = new Day10();
+    static Day11 day11 = new Day11();
+
+    public static void main(String[] args) throws FileNotFoundException {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("\nHello to Advent code 2023 year edition!");
+        System.out.println("---------------------------------------\n");
+
+//        day1.part1();
+//        day1.part2();
+//        day2.part1();
+//        day2.part2();
+//        day3.part1();
+//        day3.part2();
+//        day4.part1();
+//        day4.part2();
+//        day5.part1();
+//        day5.part2();
+//        day6.part1();
+//        day6.part2();
+//        day7.part1();
+//        day7.part2();
+//        day8.part1();
+//        day8.part2();
+//        day9.part1();
+//        day9.part2();
+//        day10.part1();
+//        day10.part2();
+//        day11.part1();
+        day11.part2();
+    }
+}
