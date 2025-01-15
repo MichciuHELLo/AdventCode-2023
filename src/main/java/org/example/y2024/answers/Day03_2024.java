5b3f6787827d3c3fbd12a2284188836274a3b5cb
me_how :) <66366313+MichciuHELLo@users.noreply.github.com>
2025-01-16 00:04:23 +0100

    feat: AdventCode day 3 (#15)

---
+++ src/main/java/org/example/y2024/answers/Day03_2024.java
@@ -0,0 +1,13 @@
+package org.example.y2024.answers;
+
+public class Day03_2024 {
+
+    final String examplePathPart1 = "src/main/java/org/example/y2024/data/d02/Day02_2024_part1_example.txt";
+    final String inputPath = "src/main/java/org/example/y2024/data/d02/Day2_2024_input.txt";
+
+    public void part1() {
+
+    }
+
+
+}
